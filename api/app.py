7c9e93c484612771f5ae92c7a0228fd808d7baa3
leeon03 <leonnutz@yahoo.com>
2025-05-29 04:44:04 +0200

    Set host and port for Render compatibility

--- api/app.py
+++ api/app.py
@@ -20,4 +20,5 @@ def get_mensa_data():
     return xml_data, 200, {'Content-Type': 'application/xml'}
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)

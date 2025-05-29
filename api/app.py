880e520662a11afaa1e2cc33522075e3ce13df13
leeon03 <leonnutz@yahoo.com>
2025-05-29 05:50:39 +0200

    Use waitress and bind to 0.0.0.0:8080 for Render

--- api/app.py
+++ api/app.py
@@ -50,5 +50,8 @@ def get_mensa_data():
 
     return jsonify(result)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+
+
